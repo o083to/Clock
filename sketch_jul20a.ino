d0a1de0e5b25444e325320a3a515b2d27f81f559
anna <bykova-anna-pavlovna@yandex.ru>
2015-07-23 23:51:05 +0300

    Время показывается. Нажатие на кнопку определяется

---
+++ sketch_jul20a.ino
@@ -0,0 +1,151 @@
+#define H4_LED_PIN 13
+#define H3_LED_PIN 12
+#define H2_LED_PIN 8
+#define DATA_PIN 7
+#define LATCH_PIN 4
+#define CLOCK_PIN 2
+#define BUZZER_PIN 3
+// Buttons
+#define LEFT_BUTTON_PIN 1
+#define RIGHT_BUTTON_PIN 0
+// Button's states
+#define UNDEFINED 0
+#define SHORT_CLICK 1
+#define LONG_CLICK 2
+#define SHORT_CLICK_MIN_DURATION 20
+#define LONG_CLICK_MIN_DURATION 700
+// RGB LED
+#define RED_PIN 9
+#define BLUE_PIN 10
+#define GREEN_PIN 11
+
+#define DELAY 1000
+#define COLORS_COUNT 5
+
+#define HOUR_LEDS_COUNT 5
+#define MINUTE_LEDS_COUNT 6
+
+const byte SINGLE_LEDS[] = { H2_LED_PIN, H3_LED_PIN, H4_LED_PIN };
+
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+
+// Colors for RGB LED
+byte r[] = { 50, 25, 2, 30, 30 };
+byte g[] = { 2, 25, 20, 20, 1 };
+byte b[] = { 2, 0, 30, 0, 20 };
+byte color = 0;
+
+// Assign current time to these variables
+byte hour = 22;
+byte minute = 43;
+byte second = 0;
+
+//Buttons variables
+boolean lButtonWasDown = false;
+boolean rButtonWasDown = false;
+unsigned long lButtonDownTime = 0;
+unsigned long rButtonDownTime = 0;
+
+void setup() {
+  pinMode(H4_LED_PIN, OUTPUT);
+  pinMode(H3_LED_PIN, OUTPUT);
+  pinMode(H2_LED_PIN, OUTPUT);
+  pinMode(DATA_PIN, OUTPUT);
+  pinMode(LATCH_PIN, OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  updateTimeLEDs();
+}
+
+void loop() {
+  checkButtons();
+  
+  currentMillis = millis();
+  if ((currentMillis - previousMillis) > DELAY) {
+//    changeRGBLEDColor();
+    updateTime();
+        
+    previousMillis = currentMillis;
+  }
+}
+
+void checkButtons() {
+  switch(checkButton(LEFT_BUTTON_PIN, &lButtonWasDown, &lButtonDownTime)) {
+    case SHORT_CLICK:
+      // test!!!
+      analogWrite(RED_PIN, 50);
+      analogWrite(GREEN_PIN, 0);
+      analogWrite(BLUE_PIN, 0); 
+      break;
+     case LONG_CLICK:
+      //test!!!
+      analogWrite(RED_PIN, 0);
+      analogWrite(GREEN_PIN, 50);
+      analogWrite(BLUE_PIN, 0); 
+  }
+}
+
+byte checkButton(byte buttonPin, boolean *buttonWasDown, unsigned long *buttonDownTime) {
+  boolean buttonIsUp = digitalRead(buttonPin);
+
+  if (!*buttonWasDown && !buttonIsUp) {
+    *buttonWasDown = true;
+    *buttonDownTime = millis();
+  } else if (*buttonWasDown && buttonIsUp) {
+    *buttonWasDown = false;
+    unsigned long clickDuration = millis() - *buttonDownTime;
+    if (clickDuration >= LONG_CLICK_MIN_DURATION) {
+      return LONG_CLICK;
+    } else if (clickDuration >= SHORT_CLICK_MIN_DURATION) {
+      return SHORT_CLICK;
+    }
+  }
+  
+  return UNDEFINED;
+}
+
+void updateTime() {
+  second++;
+  if (second == 60) {
+    second = 0;
+    minute++;
+    if (minute == 60) {
+      minute = 0;
+      hour++;
+      if (hour == 24) {
+        hour = 0;
+      }
+    }
+    updateTimeLEDs();
+  }
+}
+
+void updateTimeLEDs() {
+  byte value = 0;
+  for (byte i = 0, m = minute; i < MINUTE_LEDS_COUNT; i++, m /= 2) {
+    value |= (m % 2) << i;
+  }
+  byte h = hour;
+  for (byte i = 0; i < 2; i++, h /= 2) {
+    value |= (h % 2) << (MINUTE_LEDS_COUNT + i);
+  }
+  for (byte i = 0; i < HOUR_LEDS_COUNT - 2; i++, h /= 2) {
+    digitalWrite(SINGLE_LEDS[i], h % 2); 
+  }
+  digitalWrite(LATCH_PIN, LOW);
+  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, value);
+  digitalWrite(LATCH_PIN, HIGH);
+}
+
+void changeRGBLEDColor() {
+    analogWrite(RED_PIN, r[color]);
+    analogWrite(GREEN_PIN, g[color]);
+    analogWrite(BLUE_PIN, b[color]);    
+    color = (color == COLORS_COUNT - 1) ? 0 : color + 1;  
+}
+
+void sound() {
+  tone(BUZZER_PIN, 3500, 20);
+}
+

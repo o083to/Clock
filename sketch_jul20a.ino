c0afdff1c1fcf9d20b4de538349a861eb3cae312
anna <bykova-anna-pavlovna@yandex.ru>
2015-07-25 20:01:32 +0300

    Будильник

--- sketch_jul20a.ino
+++ sketch_jul20a.ino
@@ -1,3 +1,5 @@
+#include "enums.h"
+
 #define H4_LED_PIN 13
 #define H3_LED_PIN 12
 #define H2_LED_PIN 8
@@ -5,15 +7,11 @@
 #define LATCH_PIN 4
 #define CLOCK_PIN 2
 #define BUZZER_PIN 3
-// Buttons
 #define LEFT_BUTTON_PIN 1
 #define RIGHT_BUTTON_PIN 0
-// Button's states
-#define UNDEFINED 0
-#define SHORT_CLICK 1
-#define LONG_CLICK 2
+
 #define SHORT_CLICK_MIN_DURATION 20
-#define LONG_CLICK_MIN_DURATION 700
+#define LONG_CLICK_MIN_DURATION 500
 // RGB LED
 #define RED_PIN 9
 #define BLUE_PIN 10
@@ -21,9 +19,13 @@
 
 #define DELAY 1000
 #define COLORS_COUNT 5
+#define SOUND_DURATION 500
+#define ALARM_START_HOUR 0
+#define ALARM_START_MINUTE 0
 
 #define HOUR_LEDS_COUNT 5
 #define MINUTE_LEDS_COUNT 6
+#define SINGLE_LEDS_COUNT 3
 
 const byte SINGLE_LEDS[] = { H2_LED_PIN, H3_LED_PIN, H4_LED_PIN };
 
@@ -37,8 +39,8 @@ byte b[] = { 2, 0, 30, 0, 20 };
 byte color = 0;
 
 // Assign current time to these variables
-byte hour = 22;
-byte minute = 43;
+byte hour = 12;
+byte minute = 0;
 byte second = 0;
 
 //Buttons variables
@@ -46,6 +48,14 @@ boolean lButtonWasDown = false;
 boolean rButtonWasDown = false;
 unsigned long lButtonDownTime = 0;
 unsigned long rButtonDownTime = 0;
+
+// Alarm clock
+boolean isAlarmNow = false;
+boolean isAlarmSet = false;
+byte alarmHour = ALARM_START_HOUR;
+byte alarmMinute = ALARM_START_MINUTE;
+
+TimeLEDsMode timeLEDsMode = SHOW_TIME_MODE;
 
 void setup() {
   pinMode(H4_LED_PIN, OUTPUT);
@@ -63,30 +73,68 @@ void loop() {
   
   currentMillis = millis();
   if ((currentMillis - previousMillis) > DELAY) {
-//    changeRGBLEDColor();
+    
+    changeRGBLEDColor();
     updateTime();
+      
+    if (isAlarmNow) {
+      alarm();
+    }
         
     previousMillis = currentMillis;
   }
 }
 
 void checkButtons() {
+  checkLeftButton();
+  checkRightButton();  
+  updateTimeLEDs();
+}
+
+void checkLeftButton() {
   switch(checkButton(LEFT_BUTTON_PIN, &lButtonWasDown, &lButtonDownTime)) {
     case SHORT_CLICK:
-      // test!!!
-      analogWrite(RED_PIN, 50);
-      analogWrite(GREEN_PIN, 0);
-      analogWrite(BLUE_PIN, 0); 
+      if (timeLEDsMode == SHOW_TIME_MODE) {
+        hour = hour == 23 ? 0 : hour + 1;
+        checkAlarmClock();
+      } else {
+        isAlarmSet = true;
+        alarmHour = alarmHour == 23 ? 0 : alarmHour + 1;
+      }
       break;
      case LONG_CLICK:
-      //test!!!
-      analogWrite(RED_PIN, 0);
-      analogWrite(GREEN_PIN, 50);
-      analogWrite(BLUE_PIN, 0); 
+      if (timeLEDsMode == SHOW_TIME_MODE) {
+        timeLEDsMode = SET_ALARM_MODE;
+      } else {
+        timeLEDsMode = SHOW_TIME_MODE;
+      }      
+      break;
+  }  
+}
+
+void checkRightButton() {
+  switch(checkButton(RIGHT_BUTTON_PIN, &rButtonWasDown, &rButtonDownTime)) {
+    case SHORT_CLICK:
+      if (timeLEDsMode == SHOW_TIME_MODE) {
+        minute = minute == 59 ? 0 : minute + 1;
+        second = 0;
+        checkAlarmClock();
+      } else {
+        isAlarmSet = true;
+        alarmMinute = alarmMinute == 59 ? 0 : alarmMinute + 1;
+      }
+      break;
+    case LONG_CLICK:
+      if (timeLEDsMode == SHOW_TIME_MODE) {
+        stopAlarm();
+      } else {
+        removeAlarm();
+      }
+      break;
   }
 }
 
-byte checkButton(byte buttonPin, boolean *buttonWasDown, unsigned long *buttonDownTime) {
+ButtonState checkButton(byte buttonPin, boolean *buttonWasDown, unsigned long *buttonDownTime) {
   boolean buttonIsUp = digitalRead(buttonPin);
 
   if (!*buttonWasDown && !buttonIsUp) {
@@ -117,11 +165,26 @@ void updateTime() {
         hour = 0;
       }
     }
-    updateTimeLEDs();
+    if (timeLEDsMode == SHOW_TIME_MODE) {
+      updateTimeLEDs();
+    }
+    checkAlarmClock();
   }
 }
 
 void updateTimeLEDs() {
+  if (timeLEDsMode == SHOW_TIME_MODE) {
+    showTime(hour, minute);
+  } else {
+    if (isAlarmSet) {
+      showTime(alarmHour, alarmMinute);
+    } else {
+      showTime(31, 63);
+    }
+  }
+}
+
+void showTime(byte hour, byte minute) {
   byte value = 0;
   for (byte i = 0, m = minute; i < MINUTE_LEDS_COUNT; i++, m /= 2) {
     value |= (m % 2) << i;
@@ -130,22 +193,49 @@ void updateTimeLEDs() {
   for (byte i = 0; i < 2; i++, h /= 2) {
     value |= (h % 2) << (MINUTE_LEDS_COUNT + i);
   }
-  for (byte i = 0; i < HOUR_LEDS_COUNT - 2; i++, h /= 2) {
+  for (byte i = 0; i < SINGLE_LEDS_COUNT; i++, h /= 2) {
     digitalWrite(SINGLE_LEDS[i], h % 2); 
   }
+  writeToShiftRegister(value);
+}
+
+void writeToShiftRegister(byte value) {
   digitalWrite(LATCH_PIN, LOW);
   shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, value);
   digitalWrite(LATCH_PIN, HIGH);
 }
 
 void changeRGBLEDColor() {
+  if (timeLEDsMode == SHOW_TIME_MODE) {
     analogWrite(RED_PIN, r[color]);
     analogWrite(GREEN_PIN, g[color]);
     analogWrite(BLUE_PIN, b[color]);    
-    color = (color == COLORS_COUNT - 1) ? 0 : color + 1;  
+    color = (color == COLORS_COUNT - 1) ? 0 : color + 1;
+  } else {
+    analogWrite(RED_PIN, 50);
+    analogWrite(GREEN_PIN, 50);
+    analogWrite(BLUE_PIN, 50);
+  }
 }
 
-void sound() {
-  tone(BUZZER_PIN, 3500, 20);
+void checkAlarmClock() {
+  if (isAlarmSet) {
+    if (hour == alarmHour && minute == alarmMinute) {
+      isAlarmNow = true;
+    }  
+  }
+}
+
+void stopAlarm() {
+  isAlarmNow = false;
+}
+
+void removeAlarm() {
+  isAlarmSet = false;
+  isAlarmNow = false;
+}
+
+void alarm() {
+  tone(BUZZER_PIN, 3500, SOUND_DURATION);
 }
 
